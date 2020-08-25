d813d96c089e38b693b727e452b6b4640854637c
matchashiba <recr38@gmail.com>
2020-08-25 23:43:10 +0000

    Imported apiKey to Map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 import GoogleMapReact from 'google-map-react';
 import LocationPin from './LocationPin';
 import '../map.css';
+import api from '../api';
 
 const Map = ({ location, zoomLevel }) => {
-
+  const apiKey = api.googleKey;
 
   return (
   <div className="map">
     <h2 className="map-h2">My House</h2>
     <div className="google-map">
       <GoogleMapReact
-        bootstrapURLKeys={{key: ''}}
+        bootstrapURLKeys={{key: apiKey}}
         defaultCenter={location}
         defaultZoom={zoomLevel}
       >
