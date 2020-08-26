17de3b53665bb1196181c313c37f2d8afc86e486
matchashiba <recr38@gmail.com>
2020-08-26 19:00:44 +0000

    Installed google-maps-react to package.json Imported Map component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 
-function App() {
-
-  return (
-    <>
-      <h1>I will be a map</h1>
-    </>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Map
+          google={this.props.google}
+          zoom={8}
+          style={mapStyles}
+          initialCenter={{ lat: 21.295730, lng: -157.707370 }}
+        />
+      </>
+    );
+  }
 }
 
 export default App;
