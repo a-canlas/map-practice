0caaef1cad1fe379365784f4d0f8512c35656745
matchashiba <recr38@gmail.com>
2020-08-26 18:49:59 +0000

    Removed LocationPin and Map components Removed Map import and jsx from App

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import './App.css';
-import Map from './components/Map';
 
 function App() {
-  const location = {
-    address: '555 Hahaione St, Honolulu, HI',
-    lat: 21.295730,
-    lng: -157.707370
-  };
+
   return (
     <>
-    <Map location={location} zoomLevel={8}/>
+      <h1>I will be a map</h1>
     </>
   );
 }
